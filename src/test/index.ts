3055b96090c71be9d0a10c63c149707a21b5056c
Aex <adaex@qq.com>
2021-08-05 18:25:15 +0000

    新增文件发送测试

---
+++ src/test/index.ts
@@ -0,0 +1,1 @@
+import './test-send-file'
---
+++ src/test/test-send-file.ts
@@ -0,0 +1,24 @@
+import { $ } from 'coa-helper'
+import { resolve } from 'path'
+import { WxWorkBin, WxWorkBotService } from '..'
+
+$.run(async () => {
+  // Bin实例，任何服务都必须依赖此实例
+  const bin = new WxWorkBin()
+
+  const key = '4acd13b8-be65-4eee-90c9-7681d63c5376'
+
+  // 根据key创建一个bot服务
+  const botService = new WxWorkBotService(bin, key)
+
+  const filename = resolve(__dirname, __filename)
+
+  console.log(filename)
+
+  // 上传
+  const { mediaId } = await botService.upload(filename)
+  console.log(mediaId)
+
+  // 发送
+  await botService.file(mediaId)
+})
