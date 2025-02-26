240ba7b4bc80fd9d7395f590c339b1045c258d41
hope <hope.gao@mm-iworld.com>
2025-02-26 16:01:49 +0800

    修改批量获取客户详情接口

--- src/services/WxWorkExternalContactService.ts
+++ src/services/WxWorkExternalContactService.ts
@@ -2,10 +2,10 @@ import { WxWorkServiceAuth } from '../base/WxWorkServiceAuth'
 
 export class WxWorkExternalContactService extends WxWorkServiceAuth {
   // 批量获取客户详情 https://developer.work.weixin.qq.com/document/path/92994
-  async getByUser(userIdList: string[]) {
+  async getByUser(userIdList: string[], cursor = '', limit = 50) {
     return await this.bin.post(
       'cgi-bin/externalcontact/batch/get_by_user',
-      { userid_list: userIdList },
+      { userid_list: userIdList, cursor, limit },
       { access_token: await this.getToken() })
   }
 }
